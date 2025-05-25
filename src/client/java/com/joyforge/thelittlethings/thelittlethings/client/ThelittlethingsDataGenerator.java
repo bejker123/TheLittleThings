14de92ee6439b12d285ddcdd97f9bcec96464991
bejker123 <1piotrkucharski@gmail.com>
2025-05-25 18:23:50 +0200

    Inital commit

---
+++ src/client/java/com/joyforge/thelittlethings/thelittlethings/client/ThelittlethingsDataGenerator.java
@@ -0,0 +1,12 @@
+package com.joyforge.thelittlethings.thelittlethings.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ThelittlethingsDataGenerator implements DataGeneratorEntrypoint {
+
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+        FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+    }
+}
---
+++ src/main/java/com/joyforge/thelittlethings/thelittlethings/Thelittlethings.java
@@ -0,0 +1,10 @@
+package com.joyforge.thelittlethings.thelittlethings;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Thelittlethings implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
